5b289cffa0b15b50075231eacc50115d945dd171
shahitha99 <125952600+shahitha99@users.noreply.github.com>
2023-02-21 15:42:41 +0530

    Update streamlit_app.py
    customizing table

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,10 @@ streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-fruits_selected= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list= pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect('Pick your Fruits: ',list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected=streamlit.multiselect('Pick your Fruits: ',list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
